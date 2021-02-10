84ba8c900e9d2592c5f6cdd64fa17c572bbacac5
Misori-simon <misorisimon@gmail.com>
2021-02-11 00:11:25 +0100

    add controller methods and routes for friendship -add new_request, accept_request, decline_request methods and routes for friendship -add links to user partial for the various controls

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,26 +20,30 @@ class User < ApplicationRecord
   end
 
   def friends_group
-    friendships.where(status: true) + Friendship.where(friend_id: id, status: true)
+    friendships.where(status: true).map(&:friend) + Friendship.where(friend_id: self.id, status: true).map(&:user)
   end
 
-  def request_friend(another_user)
-    friendships.create(friend_id: another_user.id)
+  def request_friend(another_user_id)
+    self.friendships.create(friend_id: another_user_id)
   end
 
-  def accept_friend(another_user)
-    friendship = Friendship.where(user_id: another_user.id, friend_id: self.id).first
+  def accept_friend(another_user_id)
+    friendship = Friendship.where(user_id: another_user_id, friend_id: self.id, status: false).first
     friendship.status = true
     friendship.save
   end
 
-  def decline_friend(another_user)
-    freindships.where(friend_id: another_user.id, status: false).first
+  def decline_friend(another_user_id)
+    friendship = Friendship.where(user_id: another_user_id, friend_id: self.id, status: false).first
     friendship.destroy
   end
 
   def available_for_request?(another_user)
     !self.friends_group.include?(another_user) && !self.friendships.where(friend_id: another_user.id, status: false).exists?  && !Friendship.where(user_id: another_user.id, friend_id: self.id).exists?
+  end
+
+  def already_sent_request?(another_user)
+    another_user.friendships.where(friend_id: self.id, status: false).exists?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
+
+
   devise_for :users
 
   resources :users, only: [:index, :show]
+  get "friendships/new_request/friend_id" => "friendships#new_request", as: "new_request"
+  get "friendships/accept_request/friend_id" => "friendships#accept_request", as: "accept_request"
+  get "friendships/decline_request/friend_id" => "friendships#decline_request", as: "decline_request"
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]

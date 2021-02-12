030f1c5b968b8b7e8f0715560e4072191c382204
Misori-simon <misorisimon@gmail.com>
2021-02-12 07:10:24 +0100

    add test, fix errors, - add unit test for duplicate friendship creation - reverse friendship creation of the same user pair - fix rubocop errors

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,14 +4,14 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  scope :normal_friendship_order, ->(friendship) { where(user_id: friendship.user_id, friend_id: friendship.friend_id) }
-  scope :reverse_friendship_order, ->(friendship) { where(user_id: friendship.friend_id, friend_id: friendship.user_id) }
+  scope :normal_friendship_order, ->(f) { where(user_id: f.user_id, friend_id: f.friend_id) }
+  scope :reverse_friendship_order, ->(f) { where(user_id: f.friend_id, friend_id: f.user_id) }
   private
 
   def friendship_exist?
-    unless Friendship.normal_friendship_order(self).empty? && Friendship.reverse_friendship_order(self).empty?
-      errors.add(:base, "Friendship has already been sent")
-      throw(:abort)
-    end
+    return if Friendship.normal_friendship_order(self).empty? && Friendship.reverse_friendship_order(self).empty?
+
+    errors.add(:base, 'Friendship has already been sent')
+    throw(:abort)
   end
 end
